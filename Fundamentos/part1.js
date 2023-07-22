87edbef7e202ba463fb87deda407f75d0432c8c3
Audrey-Albuquerque <audreydev.br@gmail.com>
2023-07-22 10:56:42 -0300

    First commit  :woman_mechanic:

---
+++ Fundamentos/part1.js
@@ -0,0 +1,29 @@
+console.log('Only appears in the inspector ')
+
+let firstName = 'Cecília'
+let lastName = 'Tedesco'
+
+console.log(firstName +' '+ lastName)
+
+let fruit = 'apple '
+let price = 9.00
+let type = 'Gala'
+
+console.log('my favorite fruit is ' +fruit + type + ' and the price is  U$ '+ price)
+
+
+const profession = 'Developer'
+console.log('Profession: '+ profession)
+
+let Tv = 'LG 42" Oled'
+console.log('This tv is ' + Tv)
+Tv = 'Samsung 50" Led'
+console.log('This tv is ' + Tv)
+Tv = null
+console.log('This tv is ' + Tv)
+
+console.log(typeof profession)
+console.log(typeof price)
+console.log(typeof Tv)
+
+
