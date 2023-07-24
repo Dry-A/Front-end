8bcf22486f48ba143daf4fffb8891bb5bb29f185
Audrey-Albuquerque <audreydev.br@gmail.com>
2023-07-24 15:58:30 -0300

    :airplane: Fundamentos

---
+++ Fundamentos/index.js
@@ -0,0 +1,55 @@
+console.log("Only appears in the inspector ");
+
+let firstName = "Cecília";
+let lastName = "Tedesco";
+
+console.log(firstName + " " + lastName);
+
+let fruit = "apple ";
+let price = 9.0;
+let type = "Gala";
+
+console.log(
+  "My favorite fruit is " + fruit + type + " and the price is  U$ " + price
+);
+
+const profession = "Developer";
+console.log("Profession: " + profession);
+
+let Tv = 'LG 42" Oled';
+console.log("This tv is " + Tv);
+Tv = 'Samsung 50" Led';
+console.log("This tv is " + Tv);
+Tv = null;
+console.log("This tv is " + Tv);
+
+console.log(typeof profession);
+console.log(typeof price);
+console.log(typeof Tv);
+
+let pet = "dog";
+console.log("My pet is a " + pet);
+pet = "cat";
+console.log("My pet is a " + pet);
+
+let petAvailable = true;
+let petColor;
+console.log(petColor);
+petColor = "white";
+console.log("My pet's color is " + petColor);
+let petName = "Toto";
+console.log("My pet's name is " + petName);
+
+let petFriend = {
+  petColor: "black",
+  petName: "Danka",
+  petAge: 5,
+  petRace: "Lulu Pomerania",
+  petAvailable: true,
+};
+
+console.log(petFriend);
+
+petFriend.petColor = " Brown";
+
+console.log(petFriend);
