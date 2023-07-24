dd2a1bd8d2931f591152883c3d172c110ba69eb8
Audrey-Albuquerque <audreydev.br@gmail.com>
2023-07-24 16:28:23 -0300

    :jack_o_lantern: Vendo de criação de objetos

---
+++ POO/index.js
@@ -0,0 +1,11 @@
+let petFriend = {
+  petColor: "black",
+  petName: "Danka",
+  petAge: 5,
+  petRace: "Lulu Pomerania",
+  petAvailable: true,
+};
+
+console.log(petFriend);
+
+petFriend.petColor = " Brown";
