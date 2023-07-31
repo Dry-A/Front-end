c0c844fe388352050e30ab83716ae10f5a884343
Audrey-Albuquerque <audreydev.br@gmail.com>
2023-07-30 22:50:49 -0300

    :standing_woman: Exxercício de funções

---
+++ Functions/main.js
@@ -0,0 +1,34 @@
+function calculaArea(base, altura){
+
+    let area = base * altura;
+    return area;
+ }
+let base = prompt('Digite a base: ')
+base = parseInt(base)
+let altura = prompt('Digite a altura')
+altura = parseInt(altura)
+
+alert('A área é ' + calculaArea(base, altura))
+
+
+
+
+
+/*function calculaIdade(anoNascimento){
+    let idade = 2023 - anoNascimento;
+    return idade;
+}
+
+let anoNascimento = prompt('Digite o ano que voce nasceu: ')
+anoNascimento = parseInt(anoNascimento)
+
+alert('Voce tem '+ calculaIdade(anoNascimento))
+
+let nome = prompt('Digite seu nome: ')
+
+function dizOi(nomeUsuario){
+    alert ('Olá ' + nomeU)
+}
+
+dizOi()
+*/
